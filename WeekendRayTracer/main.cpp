48eb5409f9c05b6b27b656b102b55a12df2007be
SamsonStannus <samsonstannus@gmail.com>
2017-04-14 12:48:30 -0400

    Initial Commit

---
+++ WeekendRayTracer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  WeekendRayTracer
+//
+//  Created by samson stannus on 2017-04-14.
+//  Copyright (c) 2017 samson stannus. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
